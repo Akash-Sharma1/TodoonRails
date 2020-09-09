9abaf7d24f0cd035ed71ea9df923de8f96e6f827
Akash Sharma <aakashsharmajc100@gmail.com>
2020-09-09 16:03:39 +0530

    edit function added

--- todoonrails/app/controllers/todolists_controller.rb
+++ todoonrails/app/controllers/todolists_controller.rb
@@ -1,24 +1,50 @@
 class TodolistsController < ApplicationController
-    def new
-    end
     def index
         @todos = Todolist.all
     end
-     
+    
     def show
         @todo = Todolist.find(params[:id])
     end
 
+    def new
+        @todo = Todolist.new
+    end
+
+    def edit
+        @todo = Todolist.find(params[:id])
+    end
+     
     def create
         @todo = Todolist.new(todo_params)
        
-        @todo.save
-        redirect_to @todo
-      end
-       
-      private
-        def todo_params
-          params.require(:todos).permit(:description)
+        if @todo.save
+            redirect_to @todo
+        else
+            render 'new'
         end
+
+      end
     
+    def update
+        @todo = Todolist.find(params[:id])
+        
+        if @todo.update(todo_params)
+            redirect_to @todo
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @todo = todo.find(params[:id])
+        @todo.destroy
+       
+        redirect_to todos_path
+      end
+        
+    private
+        def todo_params
+            params.require(:todo).permit(:description, :priority, :duedate)
+        end
 end
